30da66822321cb11b3aedda0d2487fd36b8d6019
Quentin LOPES <quentin.lopes@orange.fr>
2026-04-22 08:25:44 +0200

    ADD/ Switcher Language (FR/EN) and js files

---
+++ script.js
@@ -0,0 +1,20 @@
+function setLanguage(lang) {
+    localStorage.setItem("lang", lang);
+
+    fetch(`./lang/${lang}.json`)
+        .then(res => res.json())
+        .then(data => {
+            document.querySelectorAll("[data-i18n]").forEach(el => {
+                const key = el.getAttribute("data-i18n");
+                if (data[key]) {
+                    el.innerHTML = data[key];
+                }
+            });
+        });
+}
+
+// Charger la langue sauvegardée au démarrage
+document.addEventListener("DOMContentLoaded", () => {
+    const savedLang = localStorage.getItem("lang") || "en";
+    setLanguage(savedLang);
+});
